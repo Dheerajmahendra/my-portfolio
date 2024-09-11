28b1beaa200efae3ecbbe5b89a28a39c46300b7b
Dheerajmahendra <122847139+Dheerajmahendra@users.noreply.github.com>
2024-09-11 09:36:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navLinks = document.querySelector('.nav-links');
+const menuItems = document.querySelectorAll('.nav-links a'); // Assuming menu items are <a> elements inside .nav-links
 
-const menuIcon = document.querySelector('#menu-icon')
-const navLinks = document.querySelector('.nav-links')
+menuIcon.onclick = () => {
+    navLinks.classList.toggle('active');
+};
 
-menuIcon.onclick =() => {
-    navLinks.classList.toggle('active')
-
-}
+menuItems.forEach(item => {
+    item.onclick = () => {
+        navLinks.classList.remove('active'); // Close the menu when a menu item is clicked
+    };
+});
